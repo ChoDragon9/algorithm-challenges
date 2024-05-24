91507faa38fc38de4864ba4b0b9c6049661a6d16
조용구 <04whdydrn30@gmail.com>
2024-05-24 23:00:30 +0900

    Create 20240524_무인도 여행.js

---
+++ 프로그래머스 Lv. 2/20240524_무인도 여행.js
@@ -0,0 +1,84 @@
+/*
+# 무인도 여행
+> https://school.programmers.co.kr/learn/courses/30/lessons/154540
+
+## 문제 설명
+메리는 여름을 맞아 무인도로 여행을 가기 위해 지도를 보고 있습니다. 지도에는 바다와 무인도들에 대한 정보가 표시돼 있습니다. 지도는 1 x 1크기의 사각형들로 이루어진 직사각형 격자 형태이며, 격자의 각 칸에는 'X' 또는 1에서 9 사이의 자연수가 적혀있습니다. 지도의 'X'는 바다를 나타내며, 숫자는 무인도를 나타냅니다. 이때, 상, 하, 좌, 우로 연결되는 땅들은 하나의 무인도를 이룹니다. 지도의 각 칸에 적힌 숫자는 식량을 나타내는데, 상, 하, 좌, 우로 연결되는 칸에 적힌 숫자를 모두 합한 값은 해당 무인도에서 최대 며칠동안 머물 수 있는지를 나타냅니다. 어떤 섬으로 놀러 갈지 못 정한 메리는 우선 각 섬에서 최대 며칠씩 머물 수 있는지 알아본 후 놀러갈 섬을 결정하려 합니다.
+
+지도를 나타내는 문자열 배열 maps가 매개변수로 주어질 때, 각 섬에서 최대 며칠씩 머무를 수 있는지 배열에 오름차순으로 담아 return 하는 solution 함수를 완성해주세요. 만약 지낼 수 있는 무인도가 없다면 -1을 배열에 담아 return 해주세요.
+*/
+
+function solution(maps) {
+    const SEA = 'X';
+    // 1. 연관 리스트 만들기
+    const graph = maps.map((map, i) => {
+        return map
+            .split('')
+            .map((str, j) => {
+                if (str === SEA) return [];
+                const arrow = [
+                    [i - 1, j], // 상
+                    [i + 1, j], // 하
+                    [i, j - 1], // 좌
+                    [i, j + 1], // 우
+                ];
+            
+                return arrow.filter(([rowIndex, colIndex]) => {
+                    if (rowIndex < 0 || rowIndex === maps.length) return false;
+                    if (colIndex < 0 || colIndex === map.length) return false;
+                    if (maps[rowIndex][colIndex] === SEA) return false;
+                    return true;
+                })
+            });
+    });
+    
+    // 2. graphInfo 만들기
+    // {num: number, visited: boolean}
+    const graphInfo = maps.map((map) => {
+        return map
+            .split('')
+            .map((str) => ({
+            num: str === 'X' ? 0 : Number(str),
+            visited: false,
+        }))
+    });
+    
+    // 3. 탐색하기
+    // sum에 해당 좌표 포함해서 누적. 
+    // 모두 완료되면 result에 추가
+    const stack = [];
+    const result = [];
+    
+    const canVisit = (col) => col.str !== SEA && !col.visited
+    
+    graphInfo.forEach((row, rowIndex) => {
+        row.forEach((col, colIndex) => {
+            if (!canVisit(col)) return; // 무인도나 방문한곳
+            let sum = 0;
+            stack.push([rowIndex, colIndex]);
+            
+            while (stack.length) {
+                const [colY, colX] = stack.pop();
+                const colInfo = graphInfo[colY][colX];
+                
+                if (!canVisit(colInfo)) continue;
+                
+                sum += colInfo.num;
+                colInfo.visited = true;
+                
+                graph[colY][colX].forEach(([linkY, linkX]) => {
+                    const info = graphInfo[linkY][linkX];
+                    if (!canVisit(info)) return;
+                    stack.push([linkY, linkX]);
+                });
+            }
+            
+            sum && result.push(sum);
+        });
+    });
+    
+    // 4. 정렬 후 반환
+    return result.length
+        ? result.sort((a, b) => a - b)
+        : [-1]
+}
